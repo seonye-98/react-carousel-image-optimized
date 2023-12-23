7322f81bb6335142da67bc05314090962a396912
선예 <kss02281@gmail.com>
2023-12-23 20:02:07 +0900

    🔧 라이브러리 빌드 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
+import * as path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   assetsInclude: ['/sb-preview/runtime.js'],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: '@seonye/test',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', '**/*.stories.tsx'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        banner: '"use client";',
+        interop: 'auto',
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  },
   plugins: [
     react({
       jsxImportSource: '@emotion/react',
       babel: {
         plugins: ['@emotion/babel-plugin'],
       },
+    }),
+    dts({
+      insertTypesEntry: true,
     }),
   ],
 });
